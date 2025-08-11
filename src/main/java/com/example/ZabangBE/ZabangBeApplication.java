32ed06e6c5a2b9077b39f95fe40ff99bcf9f318e
imkmss <signkms14@gmail.com>
2025-08-11 21:46:39 +0900

    project testing...

---
+++ src/main/java/com/example/ZabangBE/ZabangBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ZabangBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZabangBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZabangBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ZabangBE/ZabangBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZabangBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZabangBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
